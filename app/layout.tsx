04fb45d2092432a81099eccaedbb7ae3be4a1723
Thammanit <thammanitrinthang@gmail.com>
2025-01-08 14:03:55 +0700

    hover home

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,13 +9,13 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <nav
-          className="flex items-center justify-center text-white hover:underline py-3"
+          className="flex items-center justify-center text-white py-3"
           style={{
             backgroundImage: "url('https://png.pngtree.com/background/20230412/original/pngtree-nature-forest-sun-ecology-picture-image_2394782.jpg')",
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
-        ><a href="/home">Home</a>
+        ><a href="/home" className="hover:underline">Home</a>
         </nav>
         {children}
         <footer className="mt-auto text-center text-zinc-400 ">
