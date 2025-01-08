d67558d71efcfd1cb28916d93563084fb9cb6f96
Thammanit <thammanitrinthang@gmail.com>
2025-01-08 09:17:34 +0700

    GET posts err

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client'
+import { NextResponse } from 'next/server';
 
 const prisma = new PrismaClient()
 
@@ -6,9 +7,17 @@ export async function GET(
   req: Request,
   { params }: { params: { id: string } },
 ) {
-  return Response.json(await prisma.post.findUnique({
-    where: { id: Number(params.id) },
-  }))
+  try {
+    const post = await prisma.post.findUnique({
+      where: { id: Number(params.id) },
+    });
+    if (!post) {
+      return NextResponse.json({ error: 'Post not found' }, { status: 404 });
+    }
+    return NextResponse.json(post);
+  } catch (error) {
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
 }
 
 export async function PUT(
