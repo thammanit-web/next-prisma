4671b5ab615a13397adf336563a22ef27c2866e4
Thammanit <thammanitrinthang@gmail.com>
2025-01-08 10:58:12 +0700

    const { id } = await params

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -5,12 +5,12 @@ const prisma = new PrismaClient()
 
 export async function GET(
   req: NextRequest,
-  res: NextResponse,
   { params }: { params: { id: number } }
 ) {
   try {
+    const { id } = await params
     const post = await prisma.post.findUnique({
-      where: { id: Number(params.id) },
+      where: { id: Number(id) },
     });
     if (!post) {
       return NextResponse.json({ error: 'Post not found' }, { status: 404 });
@@ -22,14 +22,14 @@ export async function GET(
 }
 
 export async function PUT(
-  req: NextRequest,
-  res: NextResponse,
-  { params }: { params: { id: number } }
+  req: Request,
+  { params }: { params: { id: number } },
 ) {
   try {
+    const { id } = await params
     const { title, content } = await req.json()
     return Response.json(await prisma.post.update({
-      where: { id: Number(params.id) },
+      where: { id: Number(id) },
       data: { title, content },
     }))
   } catch (error) {
@@ -40,13 +40,13 @@ export async function PUT(
 }
 
 export async function DELETE(
-  req: NextRequest,
-  res: NextResponse,
-  { params }: { params: { id: number } }
+  req: Request,
+  { params }: { params: { id: number } },
 ) {
   try {
+    const { id } = await params
     return Response.json(await prisma.post.delete({
-      where: { id: Number(params.id) },
+      where: { id: Number(id) },
     }))
   } catch (error) {
     return new Response(error as BodyInit, {
